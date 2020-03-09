210b20807db6d8e0609f4eec63e82d1e6b93aec5
Dori Cohen <itsdori@gmail.com>
2020-03-09 09:25:01 -0700

    play around with plugins

--- video-player.js
+++ video-player.js
@@ -1,9 +1,7 @@
-var player = videojs('videoPlayer', {
-    autoplay: 'muted',
-    controls: true,
-    loop: true,
-    poster: "https://picsum.photos/800/450",
-    fluid: true,
-    aspectRatio: '4:3',
-    playbackRates: [0.5, 1, 1.5, 3]
-});
+videojs('videoPlayer').ready(function() {
+    this.hotkeys({
+      volumeStep: 0.1,
+      seekStep: 5,
+      enableModifiersForNumbers: false,
+    });
+  });
